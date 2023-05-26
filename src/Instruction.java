aaaf8185dc706a178ed31305e3e091bea74fc0b8
Mazen <mazzen.m.s0liman@gmail.com>
2023-05-26 14:00:57 +0300

    Fixed some problems and hazards

--- src/Instruction.java
+++ src/Instruction.java
@@ -1,13 +1,25 @@
 public class Instruction {
     String instruction;
+    int id;
+    String r1;
+    String r2;
+    String imm;
     String[] control = new String[9]; // control [RegDst, Branch, MemRead, MemtoReg, ALUOp, MemWrite, R1Value, ALUSrc(R2Value, Immediate), RegWrite]
 
     public Instruction(String instruction) {
         this.instruction = instruction;
     }
+    public Instruction () {}
+
+    public void setInstructionID (int id) {
+        this.id = id;
+    }
 
     public void set(String opCode, String r1, String r2, String imm, byte valR1, byte valR2, byte valImmediate) {
         // control = [RegDst, Branch, MemRead, MemtoReg (0: R, 1: I), ALUOp, MemWrite, R1Value, ALUSrc, RegWrite]
+        this.r1 = r1;
+        this.r2 = r2;
+        this.imm = imm;
         switch (opCode) {
             // R-type
             case "0000":
